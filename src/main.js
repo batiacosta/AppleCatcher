5f33a1ae7123b7a3080b7239c8f9dd69f338dac7
David Acosta <david.acosta.ing@gmail.com>
2025-03-26 16:26:27 -0500

    adding sprites for background and player

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
 import './style.css'
+import Phaser, { Physics } from 'phaser'
+
+const sizes = {
+  width: 500,
+  height: 500,
+}
+const speedDown = 300;
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game")
+    this.player
+  }
+  
+  preload(){
+    this.load.image("bg", "assets/bg.png")
+    this.load.image("basket", "assets/basket.png")
+  }
+  create(){
+    this.add.image(0, 0, "bg").setOrigin(0, 0)
+    this.player = this.physics.add.image(0, sizes.width-100, "basket").setOrigin(0, 0)
+  }
+  update(){}
+}
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene:[GameScene]
+}
+const game = new Phaser.Game(config)
